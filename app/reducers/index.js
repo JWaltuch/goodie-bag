2ede6727eb0bd8cf61efecda99651bdce1fac393
JWaltuch <jenna.waltuch@gmail.com>
2019-10-11 12:09:09 -0400

    wrote a subreducer that should theoretically get all the candies for display, assuming i am writing functionality for a thunk to grab them from the database

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,11 +1,36 @@
+import { combineReducers } from 'redux';
 
-const initialState = {}
+//action type
+const GET_CANDY = 'GET_CANDY';
+const ADD_CANDY = 'ADD_CANDY';
+
+//action creator
+const addCandy = candy => {
+  type: ADD_CANDY, candy;
+};
+const getCandy = candies => {
+  type: GET_CANDY, candies;
+};
+
+const initialState = {};
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
     default:
-      return state
+      return state;
   }
-}
+};
 
-export default rootReducer
+const candyReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_CANDY:
+      return { ...state, candies: action.candies };
+    case ADD_CANDY:
+      //functionality to add a candy
+      return state;
+    default:
+      return state;
+  }
+};
+
+export default combineReducers({ rootReducer, candyReducer });
