eadc9ea21e795c6e5fdfca5caaf32686cebca371
JWaltuch <jenna.waltuch@gmail.com>
2019-10-11 15:57:43 -0400

    added general error handler and a 404 handler

--- server/api/candies.js
+++ server/api/candies.js
@@ -1,22 +1,35 @@
 const router = require('express').Router();
 const { Candy } = require('../db/index');
 
-router.use('/:id', async (req, res, next) => {
-  try {
-    const aCandy = await Candy.findById(req.params.id);
-    res.json(aCandy);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.use('/', async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
     const allCandies = await Candy.findAll();
     res.json(allCandies);
   } catch (error) {
     next(error);
   }
+});
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const aCandy = await Candy.findById(req.params.id);
+    if (!aCandy) {
+      next();
+    } else {
+      res.json(aCandy);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.use('/', (req, res, next) => {
+  res.send('PAGE NOT FOUND');
+});
+
+router.use((error, req, res, next) => {
+  console.log(error);
+  res.send('error');
 });
 
 module.exports = router;
